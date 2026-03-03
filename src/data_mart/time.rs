995a986ea849aede8707f11e89e11f056902bdf8
Julian Bruder <julian.bruder@outlook.com>
2026-03-03 22:32:37 +0100

    generate dimension values for time

--- src/data_mart/time.rs
+++ src/data_mart/time.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
-use time::{Date, OffsetDateTime};
+use time::{Date, OffsetDateTime, macros::format_description};
+
+use crate::base_database;
 
 #[derive(Debug, Clone)]
 pub struct Time {
@@ -43,4 +45,61 @@ pub enum Weather {
     Windy,
     Miscallaneous,
     Unknown,
+}
+
+impl Time {
+    pub fn gen_times(bdb_times: Vec<base_database::time::Time>) -> Vec<Time> {
+        let month_format = format_description!("[month repr:long]");
+        bdb_times
+            .into_iter()
+            .enumerate()
+            .map(|(i, bdd_time)| Time {
+                time_id: i as u32 + 1, // start from 1 to avoid confusion with conventional uninitialized value of 0
+                timestamp: bdd_time.timestamp,
+                hier_def_day: bdd_time.timestamp.date(),
+                hier_def_month: bdd_time
+                    .timestamp
+                    .format(&month_format)
+                    .expect("shouldn't fail formatting Month from Date"),
+                hier_def_year: bdd_time.timestamp.year() as u16,
+                hier_moon_phase: bdd_time
+                    .moon_phase
+                    .map(MoonPhase::from)
+                    .unwrap_or(MoonPhase::Unknown),
+                weather: bdd_time
+                    .weather
+                    .map(Weather::from)
+                    .unwrap_or(Weather::Unknown),
+            })
+            .collect()
+    }
+}
+
+impl From<base_database::time::MoonPhase> for MoonPhase {
+    fn from(moon_phase: base_database::time::MoonPhase) -> Self {
+        match moon_phase {
+            base_database::time::MoonPhase::New => MoonPhase::New,
+            base_database::time::MoonPhase::WaxingCrescent => MoonPhase::WaxingCrescent,
+            base_database::time::MoonPhase::FirstQuarter => MoonPhase::FirstQuarter,
+            base_database::time::MoonPhase::WaxingGibbous => MoonPhase::WaxingGibbous,
+            base_database::time::MoonPhase::Full => MoonPhase::Full,
+            base_database::time::MoonPhase::WaningGibbous => MoonPhase::WaningGibbous,
+            base_database::time::MoonPhase::LastQuarter => MoonPhase::LastQuarter,
+            base_database::time::MoonPhase::WaningCrescent => MoonPhase::WaningCrescent,
+        }
+    }
+}
+
+impl From<base_database::time::Weather> for Weather {
+    fn from(weather: base_database::time::Weather) -> Self {
+        match weather {
+            base_database::time::Weather::Clear => Weather::Clear,
+            base_database::time::Weather::Cloudy => Weather::Cloudy,
+            base_database::time::Weather::RainyLight => Weather::RainyLight,
+            base_database::time::Weather::RainyHeavy => Weather::RainyHeavy,
+            base_database::time::Weather::Stormy => Weather::Stormy,
+            base_database::time::Weather::Windy => Weather::Windy,
+            base_database::time::Weather::Miscallaneous => Weather::Miscallaneous,
+        }
+    }
 }
